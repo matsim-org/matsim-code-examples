d8f30688041f0b194e9fe81ef177f3aea614360d
Kai Nagel <nagel@vsp.tu-berlin.de>
2022-01-09 19:03:44 +0100

    remove no longer used imports

--- src/main/java/org/matsim/codeexamples/extensions/freight/RunFreightExample.java
+++ src/main/java/org/matsim/codeexamples/extensions/freight/RunFreightExample.java
@@ -26,8 +26,6 @@ import org.matsim.contrib.freight.utils.FreightUtils;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
